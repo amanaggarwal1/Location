a16ed7223beda54e4a8dd38c721892aa70e598e7
amanaggarwal1 <i.aggarwalaman@gmail.com>
2020-06-30 23:44:35 +0530

    Now fetching location via location manager and location lister instead of fused location provider client in the previous version which facilitates better frequency in fetching user location.

--- app/src/main/java/com/amanaggarwal1/mapdemo/MapsActivity.java
+++ app/src/main/java/com/amanaggarwal1/mapdemo/MapsActivity.java
@@ -7,87 +7,111 @@ import androidx.fragment.app.FragmentActivity;
 import android.Manifest;
 import android.content.pm.PackageManager;
 import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
 import android.os.Bundle;
 import android.util.Log;
 import android.widget.Toast;
-import com.google.android.gms.location.FusedLocationProviderClient;
-import com.google.android.gms.location.LocationServices;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
-import com.google.android.gms.tasks.OnSuccessListener;
 
 import static com.google.android.gms.maps.model.BitmapDescriptorFactory.*;
 
 public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
 
-    private static final int REQUEST_CODE = 101;
-    private FusedLocationProviderClient fusedLocationProviderClient;
-    private Location currentLocation;
+    private LocationManager locationManager;
+    private LocationListener locationListener;
+    private SupportMapFragment mapFragment;
 
-
-    private void fetchLocation() {
-        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION)
-                != PackageManager.PERMISSION_GRANTED &&
-                ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION)
-                        != PackageManager.PERMISSION_GRANTED) {
-
-            ActivityCompat.requestPermissions(this,
-                    new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, REQUEST_CODE);
-            return;
-        }
-
-        fusedLocationProviderClient.getLastLocation().addOnSuccessListener(
-                this, new OnSuccessListener<Location>() {
-                    @Override
-                    public void onSuccess(Location location) {
-                        if (location != null) {
-                            currentLocation = location;
-                            Log.d("LOGCAT", "lat = " + location.getLatitude() + " lng = " + location.getLongitude());
-                            Toast.makeText(MapsActivity.this, "Location updated", Toast.LENGTH_SHORT).show();
-
-                            // Obtain the SupportMapFragment and get notified when the map is ready to be used.
-                            SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
-                                    .findFragmentById(R.id.map);
-                            assert mapFragment != null;
-                            mapFragment.getMapAsync(MapsActivity.this);
-                        }
-                    }
-                });
-    }
-
-
-    @Override
-    public void onMapReady(GoogleMap googleMap) {
-        googleMap.setMapType(GoogleMap.MAP_TYPE_TERRAIN);
-
-        // Add a marker in Sydney and move the camera
-        LatLng latLng = new LatLng(currentLocation.getLatitude(), currentLocation.getLongitude());
-        MarkerOptions markerOptions = new MarkerOptions().position(latLng).title("You are here")
-                .icon(defaultMarker(HUE_MAGENTA));
-        googleMap.addMarker(markerOptions);
-        googleMap.moveCamera(CameraUpdateFactory.newLatLngZoom(latLng, 17));
-        // mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
-    }
+    private static final int MIN_REQUEST_TIME = 0;
+    private static final int MIN_REQUEST_DISTANCE = 0;
 
     @Override
     public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
         super.onRequestPermissionsResult(requestCode, permissions, grantResults);
-        if (requestCode == REQUEST_CODE)
-            if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED)
-                fetchLocation();
+
+        boolean isPermissionGranted = false;
+        for (int i : grantResults)
+            if (i == PackageManager.PERMISSION_GRANTED)
+                isPermissionGranted = true;
+
+
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED &&
+                ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            if (isPermissionGranted)
+                Log.d("LOGCAT", "permission granted");
+                locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, MIN_REQUEST_TIME, MIN_REQUEST_DISTANCE, locationListener);
+                locationManager.requestLocationUpdates(LocationManager.NETWORK_PROVIDER, MIN_REQUEST_TIME, MIN_REQUEST_DISTANCE, locationListener);
+        }
     }
+
+
+    @Override
+    public void onMapReady(final GoogleMap googleMap) {
+        googleMap.setMapType(GoogleMap.MAP_TYPE_TERRAIN);
+        locationManager = (LocationManager) getSystemService(LOCATION_SERVICE);
+
+        locationListener = new LocationListener() {
+            @Override
+            public void onLocationChanged(Location location) {
+                Log.i("LOGCAT", "onLocationChanged called, lat = " +
+                        location.getLatitude() + " long = " + location.getLongitude());
+                LatLng latLng = new LatLng(location.getLatitude(), location.getLongitude());
+
+                googleMap.clear();
+                MarkerOptions markerOptions = new MarkerOptions().position(latLng).title("You are here")
+                        .icon(defaultMarker(HUE_MAGENTA));
+                googleMap.addMarker(markerOptions);
+                googleMap.moveCamera(CameraUpdateFactory.newLatLngZoom(latLng, 10));
+            }
+
+            @Override
+            public void onStatusChanged(String provider, int status, Bundle extras) {
+
+            }
+
+            @Override
+            public void onProviderEnabled(String provider) {
+                Toast.makeText(getApplicationContext(), "Enabled new provider " + provider,
+                        Toast.LENGTH_SHORT).show();
+
+            }
+
+            @Override
+            public void onProviderDisabled(String provider) {
+                Toast.makeText(getApplicationContext(), "Disabled provider " + provider,
+                        Toast.LENGTH_SHORT).show();
+            }
+        };
+
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, 1);
+        } else {
+            locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, MIN_REQUEST_TIME, MIN_REQUEST_DISTANCE, locationListener);
+            locationManager.requestLocationUpdates(LocationManager.NETWORK_PROVIDER,  MIN_REQUEST_TIME, MIN_REQUEST_DISTANCE, locationListener);
+        }
+
+    }
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_maps);
 
-        fusedLocationProviderClient = LocationServices.getFusedLocationProviderClient(this);
-        fetchLocation();
+        mapFragment = (SupportMapFragment) getSupportFragmentManager()
+                .findFragmentById(R.id.map);
+        mapFragment.getMapAsync(this);
+
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED &&
+                ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+
+            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, 1);
+        }
     }
 
 }
